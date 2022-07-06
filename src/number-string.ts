7df3894d433f8fc303425ba7c41e0bc0e9eadc5b
hondayuki <yuki.honda@rakus-partners.co.jp>
2022-07-06 15:36:23 +0900

    create boolean and number-string and array ts

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+let year: number = 1996;
+// number = false;
+console.log(year);
+
+let age: number = 0x2b;
+
+console.log(age);
+
+let name: string = 'Yuki';
+// name = 10;
+console.log(name);
